18a87bad9e17547c9667d1312d5928be25c510ef
hum-dev <humphreymuriungi@outlook.com>
2025-01-04 13:18:08 +0300

    Enhance MiniProf component: integrate user session data for profile display, add sign-in/sign-out functionality, and improve layout for better user experience

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,15 +5,17 @@ import MiniProf from './MiniProf'
 
 export default function Feed() {
   return (
-    <main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto' >
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
       {/* post */}
-      <section className='md:col-span-2' >
+      <section className="md:col-span-2">
         <Posts />
       </section>
 
       {/* MiniProf */}
-      <section className='hidden md:inline-grid md:col-span-1 ' >
-        <MiniProf />
+      <section className="hidden md:inline-grid md:col-span-1 ">
+        <div className='fixed w-[380px]'>
+          <MiniProf />
+        </div>
       </section>
     </main>
   );
--- src/components/MiniProf.jsx
+++ src/components/MiniProf.jsx
@@ -1,7 +1,28 @@
+"use client";
+import { useSession, signIn, signOut } from 'next-auth/react';
 import React from 'react'
 
 export default function MiniProf() {
+  const { data: session } = useSession();
   return (
-    <div>MiniProf</div>
-  )
+      <div className="flex items-center justify-between mt-14 ml-10">
+        <img
+          src={session?.user?.image || "/assets/800px-Instagram_logo_2016.webp"}
+          alt="user prof"
+          className="w-16 h-16 rounded-full border p-[2px] "
+        />
+
+        <div className="flex-1 ml-4 " >
+          <h2 className= " font-bold" >{session?.user?.username} </h2>
+          <h3 className='text-sm text-gray-400'> Welcome To Humsta</h3>
+        </div>
+
+        {session ? (
+        
+        <button className="text-blue-500 text-sm font-semibold " onClick={signOut}>Sign Out</button>
+        ) : (
+         <button className="text-blue-500 text-sm font-semibold" onClick={signIn}>Sign In</button>)}
+
+      </div>
+  );
 }
