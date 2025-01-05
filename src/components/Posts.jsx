d4bb3b6d1f1f3d90a7c92aa93b97849d649d9593
hum-dev <humphreymuriungi@outlook.com>
2025-01-05 12:01:46 +0300

    Add Likes component to handle post likes functionality: implement like/unlike feature, display like count, and integrate with Firestore

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,18 +5,18 @@ import Post from './Post';
 
 export default async function Posts() {
   const db = getFirestore(app);
-  const q = query(collection (db, "posts"), orderBy("timestamp", "desc"));
+  const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
   const querySnapshot = await getDocs(q);
   let data = [];
   querySnapshot.forEach((doc) => {
-    data.push({id: doc.id, ...doc.data()});
+    data.push({ id: doc.id, ...doc.data() });
   });
 
   return (
     <div>
-    {data.map((post) => (
-      <Post key={post.id} post={post} />
-    ))}
+      {data.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
 }
